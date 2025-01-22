9a31a51ee7256c15a80c7c334248dba25273aeb1
manikanta.sivakoti <manikanta.sivakoti@ivycomptech.com>
2025-01-22 14:59:34 +0530

    Initial Commit

---
+++ LFS_Demo/LFS_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LFS_DemoApp.swift
+//  LFS_Demo
+//
+//  Created by Sivakoti Sai Manikanta on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct LFS_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
